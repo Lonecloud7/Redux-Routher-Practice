9cdb775b798bfa5fd14a79cc54591e6850c97c42
Okeke Dumaga <okeke.dumaga@gmail.com>
2022-05-05 11:03:55 +0100

    change

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -1,40 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Explore = () => {
   const Navigate = useNavigate();
   const { linkAddOn } = useParams();
 
-  const [array, setArray] = useState([]);
+  const [array, setArray] = useState([{
+    balls:"these nuts"
+  }]);
+
   const key = "aec9ac0f87f04b9686f66beb5ec8d954";
-  var url = `https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2022-04-26&' +
-          'sortBy=popularity&' +
-          'apiKey=${key}`;
+  var url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aec9ac0f87f04b9686f66beb5ec8d954`;
 
-  const fetch = async (e) => {
+          const Fetch = async (e) => {
+            try {
+              const res = await fetch(url);
+              const data = await res.json();
+              setArray(data);
 
-    const res = await fetch(url)
-    const data = await res.json();
-    setArray(data)
+      
+              ;
+            } catch (err) {
+              console.log(err);
+            }
+          };
 
-    console.log(array.slice(0, 6));
-    
-  };
+  
 
   return (
     <div className="menu" style={{ textAlign: "center" }}>
       <h1>EXPLORE</h1>
 
-      <h2>Find out more</h2>
-      <form action="">
-        <input type="text" placeholder="search here" />
-      </form>
-
-      <button onClick={fetch}>FETCH!</button>
-
-      <br />
       <h2>WE ARE {linkAddOn}</h2>
       <button
         onClick={() => {
@@ -43,6 +39,17 @@ const Explore = () => {
       >
         ABOUT US
       </button>
+
+      <h2>Find out more</h2>
+      <form action="">
+        <input type="text" placeholder="search here" />
+      </form>
+
+      <button onClick={Fetch}>FETCH!</button>
+
+      <br />
+      
+      
     </div>
   );
 };
