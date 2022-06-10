1c5366634c3346a731b8694eede3daf17acfedf9
Okeke Dumaga <okeke.dumaga@gmail.com>
2022-06-10 15:59:10 +0100

    changes

--- pages/aboutus.jsx
+++ pages/aboutus.jsx
@@ -11,18 +11,46 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
+import Checkbox from '@mui/material/Checkbox';
 import Box from "@mui/material/Box";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from "@mui/material";
+
+import { makeStyles } from "@mui/material";
 
 const AboutUs = () => {
   const [value, setValue] = useState("");
 
+  const [checked, setChecked] = useState(false);
+
+  const [checkState, setCheckState] = useState(false);
+
   const Dispatch = useDispatch();
+
+  // const useStyles = makeStyles({
+  //   root:{
+  //     background: "red",
+  //     color:"blue",
+  //   }
+  // })
+
+  // const TestColor = () => {
+  //   const classes = useStyles();
+  //   return <Button className={classes.root}> LETS SEE </Button>
+  // }
 
   const { addValue, deleteValue } = bindActionCreators(
     actionCreators,
     Dispatch
   );
+
+  const handleCheck = (e) => {
+    setChecked(e.target.check)
+
+    setCheckState((prev) => {
+      return !prev
+    })
+  }
 
   const ArrayReducer = useSelector((state) => state.ArrayState);
 
@@ -37,6 +65,10 @@ const AboutUs = () => {
     <div className="aboutus" style={{ textAlign: "center" }}>
       <h1>ABOUT US</h1>
       <br />
+
+      {/* <TestColor />
+
+      <br /> */}
 
 
       <h2>STATE 3:</h2>
@@ -56,23 +88,31 @@ const AboutUs = () => {
       </form>
 
       <div>
-        {ArrayReducer[0] && <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "black" }}>
+        {ArrayReducer[0] && <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "gray" }}>
           <List>
             {ArrayReducer.map((value, index) => {
               return (
                 <nav aria-label="main mailbox folders" key={index} id={index}>
-                  <ListItem Padding>
-                    <ListItemButton>
-                      <ListItemIcon></ListItemIcon>
-                      <ListItemText primary={value} />
-                    </ListItemButton>
-                    <Button
-                      onClick={() => {
+                  <ListItem 
+                  secondaryAction={
+              <IconButton edge="end" >
+                <DeleteIcon 
+                  onClick={() => {
                         deleteValue(index);
                       }}
-                    >
-                      DELETE
-                    </Button>
+                />
+              </IconButton>
+            }
+                  Padding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <Checkbox
+                          edge="start"
+                          onChange={handleCheck}
+                         />
+                      </ListItemIcon>
+                      <ListItemText primary={value} style={{ textDecoration: checkState ?"line-Through" : "none" }}/>
+                    </ListItemButton>
                   </ListItem>
                 </nav>
               );
